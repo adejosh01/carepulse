1819c902f99dea5f9c4e8b6765ddd80bb9b10105
Adeloba Joshua <adelobajoshua19@gmail.com>
2024-07-11 15:45:42 +0100

    register and onboarding

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "../libs/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,7 +25,11 @@ export default function RootLayout({
       <body className={cn(
           "min-h-screen bg-dark-300 font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
